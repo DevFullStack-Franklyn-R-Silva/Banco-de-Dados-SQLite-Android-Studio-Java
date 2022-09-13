c14591ca1f0f40a6e29dd58e547a466e9b31ec1b
Franklyn R <franklyn1998@live.com>
2022-09-13 16:35:26 -0300

    Aplicando filtros - parte 1

--- app/src/main/java/com/hadesfranklyn/bancodedadossqlite/MainActivity.java
+++ app/src/main/java/com/hadesfranklyn/bancodedadossqlite/MainActivity.java
@@ -23,21 +23,36 @@ public class MainActivity extends AppCompatActivity {
             bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas(nome VARCHAR, idade INT(3))");
 
             //Inserir dados
-            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Franklyn', 24)");
-            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Carlos', 30)");
-            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Maria', 35)");
+//            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Franklyn', 24)");
+//            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Carlos', 30)");
+//            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Maria', 35)");
 
             //Recuperar pessoas
-            Cursor cursor = bancoDados.rawQuery("SELECT nome, idade FROM pessoas", null);
+//            String consulta =
+//                    "SELECT nome, idade FROM pessoas " +
+//                    "WHERE nome = 'Franklyn' AND idade = 24";
+//            String consulta =
+//                    "SELECT nome, idade FROM pessoas " +
+//                    "WHERE idade >= 30 OR idade = 24";
+
+            String consulta =
+                    "SELECT nome, idade FROM pessoas " +
+                    "WHERE idade IN(24,30)";
+
+            Cursor cursor = bancoDados.rawQuery(consulta, null);
 
             //Indices da tabela
             int indiceNome = cursor.getColumnIndex("nome");
             int indiceIdade = cursor.getColumnIndex("idade");
-            
+
             cursor.moveToFirst();
-            while(cursor != null){
-                Log.i("Resultado - nome: ", cursor.getString(indiceNome));
-                Log.i("Resultado - idade: ", cursor.getString(indiceIdade));
+            while (cursor != null) {
+
+                String nome = cursor.getString(indiceNome);
+                String idade = cursor.getString(indiceIdade);
+
+                Log.i("Resultado - nome ", nome + " / idade: " + idade);
+
                 cursor.moveToNext();
             }
 
