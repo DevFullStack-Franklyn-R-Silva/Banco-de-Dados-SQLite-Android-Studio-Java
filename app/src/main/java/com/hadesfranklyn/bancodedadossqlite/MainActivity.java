184262f4a27e7957e2b62748aaa7c915b5850cd8
Franklyn R <franklyn1998@live.com>
2022-09-13 16:22:44 -0300

    Banco de Dados - SQLite

--- app/src/main/java/com/hadesfranklyn/bancodedadossqlite/MainActivity.java
+++ app/src/main/java/com/hadesfranklyn/bancodedadossqlite/MainActivity.java
@@ -1,8 +1,11 @@
 package com.hadesfranklyn.bancodedadossqlite;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+
+            //Criar banco de dados
+            SQLiteDatabase bancoDados = openOrCreateDatabase("app", MODE_PRIVATE, null);
+
+            //Criar tabela
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas(nome VARCHAR, idade INT(3))");
+
+            //Inserir dados
+            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Franklyn', 24)");
+            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Carlos', 30)");
+            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Maria', 35)");
+
+            //Recuperar pessoas
+            Cursor cursor = bancoDados.rawQuery("SELECT nome, idade FROM pessoas", null);
+
+            //Indices da tabela
+            int indiceNome = cursor.getColumnIndex("nome");
+            int indiceIdade = cursor.getColumnIndex("idade");
+            
+            cursor.moveToFirst();
+            while(cursor != null){
+                Log.i("Resultado - nome: ", cursor.getString(indiceNome));
+                Log.i("Resultado - idade: ", cursor.getString(indiceIdade));
+                cursor.moveToNext();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
